8b26210cafa4c7c1fd495332d340323dc2aaf11e
AnkitG0204 <42875414+AnkitG0204@users.noreply.github.com>
2026-01-09 02:31:52 +0000

    Adding test cases for corenlp

--- test_corenlp.py
+++ test_corenlp.py
@@ -1,5 +1,15 @@
-from nlplogic.corenlp import get_phrases
-
+from nlplogic.corenlp import get_phrases, search_wikipedia, get_text_blob, summarize_wikipedia
+import wikipedia
+from textblob import TextBlob
 
 def test_get_phrase():
     assert "golden state" in get_phrases("Golden State Warriors")
+
+def test_summarize_wikipedia():
+    assert wikipedia.summary("Golden State Warriors") == summarize_wikipedia("Golden State Warriors")
+
+def test_get_text_blob():
+    assert TextBlob(wikipedia.summary("Golden State Warriors")) == get_text_blob(wikipedia.summary("Golden State Warriors"))
+
+def test_search_wikipedia():
+    assert search_wikipedia("Golden State Warriors") == wikipedia.search("Golden State Warriors")
